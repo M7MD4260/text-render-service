3e6e133c539e85a58da27e9a32f34e550bf9e672
MO7AMMED <hhakem256@gmail.com>
2026-07-16 03:27:03 +0300

    Refactor text rendering service for Arabic support

--- text-render-service/text-service.js
+++ text-render-service/text-service.js
@@ -118,13 +118,13 @@ app.post("/render", async (req, res) => {
 
       #name {
         font-family:
-          'NotoJP',          /* ياباني وكاتاكانا وهيراغانا */
-          'NotoTibetan',    /* رموز التبت مثل ࿔ */
-          'NotoSymbols1',   /* رموز Unicode العامة */
-          'NotoSymbols2',   /* زخارف ورموز إضافية */
-          'NotoMath',       /* الرموز الرياضية */
-          'NotoArabic',     /* العربي */
-          'NotoSans',       /* إنجليزي ولاتيني */
+          'NotoJP',
+          'NotoTibetan',
+          'NotoSymbols1',
+          'NotoSymbols2',
+          'NotoArabic',
+          'NotoSans',
+          'NotoMath',
           sans-serif;
 
         font-size: 60px;
